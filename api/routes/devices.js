f8d587078f22b97b09efac1daa8e24a00023b361
jonnathan <jrqvjre@gmail.com>
2022-01-20 17:31:25 -0500

    enero20

--- api/routes/devices.js
+++ api/routes/devices.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/testing", (req, res) => {
-  res.send("Hello IoT GL API from devices.js");
+  res.send("Hello IoT GL API from devicesssssssss.js");
 });
 
 module.exports = router;
